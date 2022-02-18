e476e7b3be85d141e185a7f886ab7905f20ca865
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2022-02-18 08:20:05 -1100

    Added second passenger form

--- js/modules/formPerson.js
+++ js/modules/formPerson.js
@@ -31,6 +31,26 @@ const createFormPerson = (n) => {
         required: true,
     });
 
+    const fieldTicket = createElement('div', {
+        className: 'field',
+    });
+
+    const labelTicket = createElement('label', {
+        className: 'field__label',
+        textContent: 'Номер билета (10 цифр)',
+    });
+
+    const inputTicket = createElement('input', {
+        className: 'field__input',
+        id: `ticket${n}`,
+        name: 'ticket',
+        type: 'text',
+        placeholder: 'Номер билета',
+        required: true,
+        minLength: 10,
+        maxLength: 10,
+    })
+
     const button = createElement('button', {
         className: 'btn-confirm',
         type: 'submit',
@@ -38,7 +58,8 @@ const createFormPerson = (n) => {
     });
 
     fieldName.append(labelName, inputName);
-    form.append(title, fieldName, button);
+    fieldTicket.append(labelTicket, inputTicket);
+    form.append(title, fieldName, fieldTicket, button);
 
     return form;
 }
